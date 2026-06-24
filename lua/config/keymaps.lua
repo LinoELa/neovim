c9a70611e34414a1170aa9e4172304d4684ad9a2
Paulino Salvador Omnitec <paulino.salvador@omnitecsystems.com>
2026-06-24 08:54:45 +0200

    feat: agregar atajos para mover líneas y selecciones en modo normal y visual

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,13 @@ map("n", "J", "6j", { desc = "Bajar 6 lineas" })
 map("n", "K", "6k", { desc = "Subir 6 lineas" })
 map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Buffer anterior" })
 map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Buffer siguiente" })
+map("n", "<A-Down>", ":m .+1<CR>==", { desc = "Mover linea abajo" })
+map("n", "<A-Up>", ":m .-2<CR>==", { desc = "Mover linea arriba" })
+map("v", "<A-Down>", ":m '>+1<CR>gv=gv", { desc = "Mover seleccion abajo" })
+map("v", "<A-Up>", ":m '<-2<CR>gv=gv", { desc = "Mover seleccion arriba" })
+
+-- Si tu terminal no detecta Alt correctamente, prueba estas variantes:
+-- map("n", "<C-Down>", ":m .+1<CR>==", { desc = "Mover linea abajo" })
+-- map("n", "<C-Up>", ":m .-2<CR>==", { desc = "Mover linea arriba" })
+-- map("v", "<C-Down>", ":m '>+1<CR>gv=gv", { desc = "Mover seleccion abajo" })
+-- map("v", "<C-Up>", ":m '<-2<CR>gv=gv", { desc = "Mover seleccion arriba" })
